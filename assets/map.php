f87a566314727822687b07f7fa79254d787ee36e
mouafikmohammed <mouafikmohammed03@gmail.com>
2022-11-11 07:13:04 +0100

    fetch, insert, update

---
+++ assets/map.php
@@ -0,0 +1,15 @@
+<section class='map'>
+         <h1>Map</h1>
+        <span class="map_location">
+                <p>Home</p>
+                <i class="fas fa-chevron-right"></i>
+                <p class="all_map_para">Map</p> 
+        </span>
+        <div class="show_map">
+            <h1>School Location</h1>
+            <div class='show_map_inner'>
+              <iframe src="https://www.google.com/maps/..." style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+         </div>
+         <h2>Islamic School System</h2>
+</section>
